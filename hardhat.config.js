ae154976cf1a88fd5621f9d2c1182f40932fcbcd
Dimitris Lamprinos <pkakelas@gmail.com>
2023-03-04 13:48:57 -0700

    Add matic deployment config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,6 +8,11 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
+    },
+    matic: {
+      url: "https://polygon-testnet.public.blastapi.io",
+      accounts: [process.env.PK],
+      chainId: 80001,
     }
   }
 };
